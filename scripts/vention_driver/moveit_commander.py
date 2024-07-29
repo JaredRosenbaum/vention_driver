c0d41fcd34b4c255449f98abf2120e99bfa2cee6
Jared Rosenbaum <jaredsr2@utexas.edu>
2024-07-29 11:11:36 -0500

    rework

--- scripts/vention_driver/moveit_commander.py
+++ scripts/vention_driver/moveit_commander.py
@@ -1,8 +1,8 @@
 #! /usr/bin/env python
 ##############################################################################
-#      Title     : moveit_commander.py
+#      Title     : follow_joint_trajectory_action_server.py
 #      Project   : vention_driver
-#      Created   : 07/29/2024
+#      Created   : 05/15/2024
 #      Author    : Jared Rosenbaum
 #      Copyright : Copyright© The University of Texas at Austin, 2024-2030. All
 #      rights reserved.
@@ -53,20 +53,39 @@ import moveit_commander
 import moveit_msgs.msg
 import geometry_msgs.msg
 from std_msgs.msg import String
-# from moveit_commander.conversions import pose_to_list
-
+from moveit_commander.conversions import pose_to_list
 
 
 #TODO If namespace changes, must be reflected here. Or make it a variable pulled in somehow.
-class MoveitThread():
+class FollowJointTrajectory():
     def __init__(self, argv):
         # Any vention related stuff goes here
-        print("Moveit commander active")
+        print("moveit commander node up")
 
+        # Initialize subscriber to action server+ Joint state publisher
+        #! NOTE: THIS METHOD WILL ONLY WORK IN ROS 1. IF PORTING TO ROS 2, WITH THE CHANGES IN HOW ACTIONS WORK, A NEW METHOD WILL BE REQUIRED.
+    
+        # self.joint_state_pub = rospy.Publisher(
+        #     "/dsr01dootion/joint_states", JointState, queue_size=10
+        # )
+
+        # self.moveit_to_humble_pub = rospy.Publisher(
+        #     '/dsr01dootion/dsr_joint_trajectory_controller/humble_command', JointTrajectory, queue_size=10
+        # )
+        # self.servo_to_arm_pub = rospy.Publisher(
+        #     '/dsr01dootion/dsr_joint_trajectory_controller/follow_joint_trajectory/goal', FollowJointTrajectoryActionGoal, queue_size=10
+        # )
+
+        # rospy.Subscriber("/dsr01dootion/dsr_joint_trajectory_controller/follow_joint_trajectory/goal", FollowJointTrajectoryActionGoal, self.moveit_callback)
+        # rospy.Subscriber("/joint_group_position_controller/command", JointTrajectory, self.servo_callback)
+        # rospy.Subscriber("/dsr01dootion/joint_states", JointState, self.updatestates_callback)
         rospy.Subscriber("/surface_repair2/moveitPoseGoal", Pose, self.moveitToGoal)
 
 
         # Instantiate variables
+        self.states = 0
+        self.ventionstate = 0
+        self.doosanstate = 0
         self.rate = rospy.Rate(2)
 
         moveit_commander.roscpp_initialize(sys.argv)
@@ -75,7 +94,52 @@ class MoveitThread():
         group_name = "doosan_only"
         self.move_group = moveit_commander.MoveGroupCommander(group_name)
 
+        
+    def publish_state(self):
+        #TODO Is there any reason not to completely redo this, and instead have the vention wrapper intercept joint state messages and complete them?
+        rospy.wait_for_service('jointservice')
+        joint_state = JointState()
+        while not rospy.is_shutdown():
+            test = rospy.ServiceProxy('jointservice', Empty)
+            #todo add error handling? note: Maybe not
+            #todo need a way to not crash if the servo input is cancelled. need to track down where thats coming from
+            #! This carries on to 
+            test()
+            #todo get joint states through vention driver
+            # joint_state.header.stamp = rospy.get_rostime()
+            # joint_state.name = ["tower_prismatic"]
+            # joint_state.position = [0]
+            # joint_state.velocity = [0]
+            # joint_state.effort = [0]
+            # self.joint_state_pub.publish(joint_state) 
+            self.rate.sleep()
+
+    def moveit_callback(self, data):
+        self.moveit_to_humble_pub.publish(data.goal.trajectory)
+        return
+        
+
+    # TODO for commands with vention only, populate message with current arm positions
+    def servo_callback(self, data):
+        message = FollowJointTrajectoryActionGoal()
+        print(data.points[0].positions)
+        message.goal.trajectory = data
+        #todo states must be wrong...
+        if (abs(sum(data.points[0].positions)-self.states) > 0.00001):
+            print(abs(sum(data.points[0].positions)-self.states))
+            self.servo_to_arm_pub.publish(message)
+            print("Message published")
+        return
     
+    def updatestates_callback(self, data):
+        #todo handle cases for arm only or vention only
+        # if vention, update vention #, if doosan, update doosan #, then sum thte two at the end
+        if len(data.name) == 1:
+            self.ventionstate = sum(data.position)
+        else:
+            self.doosanstate = sum(data.position)
+        self.states = self.ventionstate + self.doosanstate
+        return
     
 
     #* A geometry_msgs/pose is receivied, published by ros2. That message should then be packaged up into moveit and 
@@ -99,15 +163,26 @@ class MoveitThread():
         
 
 
-def main():
-    # Initialize the ROS node
-    rospy.init_node('moveit_node', anonymous=True)
+def main(argv):
 
-    # Create an instance of your MoveitThread class
-    moveit_thread = MoveitThread(sys.argv)
+    follow_joint_trajectory = FollowJointTrajectory(argv)
+    joint_states_pub_thread = threading.Thread(
+        target=follow_joint_trajectory.publish_state()
+    )
+    joint_states_pub_thread.start()
 
-    # Keep the node running
-    rospy.spin()
 
-if __name__ == '__main__':
-    main()
+    while not rospy.is_shutdown():
+        rospy.spin()
+
+    joint_states_pub_thread.join()
+    follow_joint_trajectory.disconnect()
+
+
+if __name__ == "__main__":
+    rospy.init_node("moveit_commander_node")
+    
+    reload(logging)
+    argv = rospy.myargv(argv=sys.argv)
+    if not main(argv[1:]):
+        sys.exit(1)
