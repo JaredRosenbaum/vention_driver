3fb6f539c25249595c93dc4cbf9949e805e7dccf
Jared Rosenbaum <jaredsr2@utexas.edu>
2024-07-29 11:03:50 -0500

    updated cmake and package

--- scripts/vention_driver/moveit_commander.py
+++ scripts/vention_driver/moveit_commander.py
@@ -53,7 +53,7 @@ import moveit_commander
 import moveit_msgs.msg
 import geometry_msgs.msg
 from std_msgs.msg import String
-from moveit_commander.conversions import pose_to_list
+# from moveit_commander.conversions import pose_to_list
 
 
 
